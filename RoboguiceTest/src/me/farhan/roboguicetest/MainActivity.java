83ab70bbd24a59c1039d34ca291a46ca7944c9fc
farhan-faruque <russel.cse05@gmail.com>
2014-01-30 00:19:48 +0600

    roboguice test completed

--- RoboguiceTest/src/me/farhan/roboguicetest/MainActivity.java
+++ RoboguiceTest/src/me/farhan/roboguicetest/MainActivity.java
@@ -1,15 +1,27 @@
 package me.farhan.roboguicetest;
 
+import roboguice.activity.RoboActivity;
+import roboguice.inject.InjectResource;
+import roboguice.inject.InjectView;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
-import android.app.Activity;
 import android.view.Menu;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-public class MainActivity extends Activity {
+public class MainActivity extends RoboActivity {
+
+	@InjectView(R.id.text) TextView name;
+	@InjectView(R.id.imageView1) ImageView thumbnail;
+	@InjectResource(R.drawable.ic_launcher) Drawable icon;
+	@InjectResource(R.string.app_name) String myName;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		name.setText(myName);
 	}
 
 	@Override
